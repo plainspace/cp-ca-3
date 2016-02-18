d3bf3ca95821817bbf0f62a60945a78135a3dc79
Jared Volpe <plainspace@gmail.com>
2016-02-17 20:26:15 -0800

    Initial commit

---
+++ canvas/CanvasViewController.swift
@@ -0,0 +1,225 @@
+//
+//  CanvasViewController.swift
+//  canvas
+//
+//  Created by Jared on 2/17/16.
+//  Copyright © 2016 plainspace. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+    
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
+    // Define var to hold the initial point position of the new faces.
+    var initialCenter: CGPoint!
+    
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    // variables to hold the transform values for scale and rotation.
+    var scale = CGFloat(1.0)
+    var rotation = CGFloat(0)
+    
+    @IBAction func didPanTray(sender: AnyObject) {
+    }
+    @IBOutlet weak var trayImageView: UIView!
+    @IBOutlet weak var trayArrow: UIView!
+    
+    
+    @IBAction func didPanFace(sender: AnyObject) {
+        
+        let imageView = sender.view as! UIImageView
+        
+        let translation = sender.translationInView(view)
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            view.addSubview(newlyCreatedFace)
+            
+            newlyCreatedFace.center = imageView.center
+            
+            newlyCreatedFace.center.y += trayImageView.frame.origin.y
+
+            // set the initial center point
+            initialCenter = newlyCreatedFace.center
+            
+            // Enable userInteraction so the gesture recognizers can function
+            newlyCreatedFace.userInteractionEnabled = true
+            
+            // Create a new pan gesture recognizer that calls the function "didPanFaceCanvas"
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "didPanFaceCanvas:")
+            
+            // Add the pan gesture regognizer you created to the newly created face
+            newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
+            
+            // animate the newlycreated face to scale up 2.4x
+            UIImageView.animateWithDuration(0.2, animations: { () -> Void in
+                self.newlyCreatedFace.transform = CGAffineTransformMakeScale(2.4, 2.4)
+            })
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+        
+            newlyCreatedFace.center = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            
+            if newlyCreatedFace.center.y >= trayImageView.frame.origin.y {
+                
+                // Animate the newlyCreatedFace back to it's original position.
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.newlyCreatedFace.center = self.initialCenter
+                    self.newlyCreatedFace.transform = CGAffineTransformMakeScale(1, 1)
+                    }, completion: { (Bool) -> Void in
+                        
+                        // When the animation finishes, remove the newly created face
+                        self.newlyCreatedFace.removeFromSuperview()
+                })
+                
+            } else {
+                
+                // otherwise, the newly created face has been moved above the tray. Animate it to scale slightly down to 2x
+                UIImageView.animateWithDuration(0.2, animations: { () -> Void in
+                    self.newlyCreatedFace.transform = CGAffineTransformMakeScale(2, 2)
+                })
+            }
+        }
+    }
+    
+    // function that is called from the pan gesture recognizer
+    func didPanFaceCanvas(panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        // Get the translation from the pan gesture recognizer
+        let translation = panGestureRecognizer.translationInView(view)
+        
+        // The moment the gesture starts...
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+            // reference the ImageView that recieved the gesture (the face you panned) and store it in newlyCreatedFace
+            newlyCreatedFace = panGestureRecognizer.view as! UIImageView
+            
+            // set the initial center point
+            initialCenter = newlyCreatedFace.center
+            
+            // bring the NewlyCreatedFace imageView to the front
+            newlyCreatedFace.superview?.bringSubviewToFront(view)
+            
+            // while the user is in the process of panning. (called continuously as user pans)
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            
+            // move the face with the pan
+            newlyCreatedFace.center = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            
+            // When the user has stopped panning
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            print("panning face ended")
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "didPanFace:")
+        
+        // trayDownOffset = 160
+        // trayUp = trayImageView.center
+        // trayDown = CGPoint(x: trayImageView.center.x ,y: trayImageView.center.y + trayDownOffset)
+        
+        // Assign values to the tray up and tray down variables we defined above.
+        trayUp = CGPoint(x: 0, y: 348)
+        trayDown = CGPoint(x: 0, y: 530)
+        
+        // Set the tray view to start in the down position.
+        trayImageView.frame.origin = trayDown
+        
+        // set the tray arrow to be facing up (original image faces down)
+        // trayArrow.transform = CGAffineTransformMakeRotation(CGFloat(180 * M_PI / 180))
+        
+        // set the friction drag value. We will divide the translation value by this to implement a friction drag.
+        // frictionDrag = 10
+
+
+        // The onCustomPan: method will be defined in Step 3 below.
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "onTrayPan:")
+        
+        // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+        
+        trayImageView.userInteractionEnabled = true
+        trayImageView.addGestureRecognizer(panGestureRecognizer)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func onTrayPan(sender: UIPanGestureRecognizer) {
+        let point = sender.locationInView(view)
+        let velocity = sender.velocityInView(view)
+        let translation = sender.translationInView(view)
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+            
+            trayOriginalCenter = trayImageView.center
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+            
+            trayImageView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+            
+            // If the user was panning up when the gesture ended...Move the tray to it's up position.
+            if velocity.y < 0 {
+                
+                // Animate the tray View to it's up position and spin the arrow imageView 0 degrees
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+                    
+                    // Move tray to up position
+                    self.trayImageView.frame.origin = self.trayUp
+                    
+                    // Rotate the arrow back to it's original position.
+                    // self.trayArrow.transform = CGAffineTransformMakeRotation(0)
+                    
+                    }, completion: nil)
+                
+                // otherwise, animate the tray position to the down position and spin the arrow 180 degrees
+            } else {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+                    
+                    // Move tray to down position
+                    self.trayImageView.frame.origin = self.trayDown
+                    
+                    // rotate the arrow 180 degrees
+                    // self.trayArrow.transform = CGAffineTransformMakeRotation(CGFloat(180 * M_PI / 180))
+                    
+                    }, completion: nil)
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
